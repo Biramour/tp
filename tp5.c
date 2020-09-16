3ff8c5fc7499850a6708232eec90bfe7ccbf90dd
BD <jiinshu@yahoo.fr>
2020-09-16 22:18:04 +0200

    figures

--- tp5.c
+++ tp5.c
@@ -2,8 +2,10 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <math.h>
+#include <string.h>
 #include "tp5.h"
 #define prl {printf("\n");}
+#define square(a)  (a)*(a)
 
 /***************************************************/
 
@@ -31,13 +33,139 @@ coord_t inv_rotate_point(coord_t p, double t)
     return coordinate(p.x*cos(-t)-p.y*sin(-t), p.x*sin(-t)+p.y*cos(-t) );
 }
 
+
+figure_t circle(double radius)
+{
+    figure_t new;
+    new.angle = 0;
+    new.shape = CIRCLE;
+    new.color = '+';
+    new.caracteristic.circle.radius = radius;
+
+    return new;
+}
+
+figure_t rectangle(double width, double height)
+{
+    figure_t new;
+    new.angle = 0;
+    new.shape = RECTANGLE;
+    new.color = '+';
+    new.caracteristic.rectangle.width = width;
+    new.caracteristic.rectangle.height = height;
+
+    return new;
+}
+
+figure_t line(double length)
+{
+    figure_t new;
+    new.angle = 0;
+    new.shape = LINE;
+    new.color = '+';
+    new.caracteristic.line.length = length;
+
+    return new;
+}
+
+figure_t color(figure_t f, color_t c)
+{
+    f.color = c;
+    return f;
+}
+
+coord_t coord_translate(coord_t p, coord_t v)
+{
+    return coordinate(p.x+v.x,p.y+v.y);
+}
+
+figure_t translate(figure_t f, double dx, double dy)
+{
+    f.center = coord_translate(f.center,coordinate(dx,dy));
+    return f;
+}
+
+figure_t rotate(figure_t f, double dt)
+{
+    f.angle = f.angle + dt;
+    return f;
+}
+
+color_t intersect(coord_t p, figure_t f, double grain)
+{
+    p = inv_translate_point(p,f.center);
+    p = inv_rotate_point(p,f.angle);
+
+    switch (f.shape)
+    {
+    case CIRCLE:
+        if(square(p.x)+square(p.y) <= f.caracteristic.circle.radius)
+        {
+            return f.color;
+        }
+        return  0;
+    break;
+    case RECTANGLE:
+        if(fabs(p.x)<=f.caracteristic.rectangle.width && fabs(p.y) <= f.caracteristic.rectangle.height)
+        {
+            return f.color;
+        }
+        return 0;
+    break;
+    case LINE:
+        if(fabs(p.x)<= f.caracteristic.line.length && fabs(p.y) <= grain)
+        {
+            return f.color;
+        }
+        return 0;
+    break;
+    default:
+        break;
+    }
+
+}
 /**************************************************/
 
 int main() {
-  coord_t pt = coordinate(10,20);
-  print_coord(pt); prl;
-  print_coord(inv_translate_point(pt,pt)); prl;
-  print_coord(inv_rotate_point(pt,M_PI_2)); prl;
-  print_coord(inv_rotate_point(pt,-M_PI_4)); prl;
+  figure_t f;
+  color_t c;
+ 
+  f = rectangle(10,7);
+  f = translate(f, 10, 8);
+  f = rotate(f, 45 * M_PI / 180);
+
+  printf("h= %f et w =%f",f.caracteristic.rectangle.height, f.caracteristic.rectangle.width);
+  prl;
+ 
+  c = intersect(coordinate(5,5),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+  c = intersect(coordinate(10,10),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+  c = intersect(coordinate(18,18),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+ 
+  f = circle(12);
+  f = translate(f, 20, 13);
+  f = color(f, 'O');
+ 
+  c = intersect(coordinate(5,5),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+  c = intersect(coordinate(10,10),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+  c = intersect(coordinate(18,18),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+ 
+  f = line(20);
+  f = translate(f, 18, 18);
+  f = rotate(f, -60 * M_PI / 180);
+  f = color(f, '*');
+ 
+  c = intersect(coordinate(5,5),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+  c = intersect(coordinate(10,10),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+  c = intersect(coordinate(18,18),f,1);
+  if (c) printf("Intersection with color %c\n",c); else printf("No intersection\n");
+ 
   return EXIT_SUCCESS;
 }
--- tp5.h
+++ tp5.h
@@ -14,7 +14,7 @@ typedef struct
 
 typedef struct 
 {
-    double length;
+    double height;
     double width;
 } rectangle_t;
 
@@ -32,10 +32,38 @@ typedef enum
 
 typedef char color_t;
 
+typedef union
+{
+   rectangle_t rectangle;
+   line_t line;
+   circle_t circle;
+}caracteristic_t;
+
+
+typedef struct
+{
+    color_t color;
+    coord_t center;
+    double angle;
+    shape_t shape;
+    caracteristic_t caracteristic;
+
+}figure_t;
+
+
 coord_t coordinate(double x, double y);
 void print_coord(coord_t coord);
 coord_t inv_translate_point(coord_t p, coord_t v);
 coord_t inv_rotate_point(coord_t p, double t);
+coord_t coord_translate(coord_t p, coord_t v);
+figure_t circle(double radius);
+figure_t rectangle(double width, double height);
+figure_t line(double length);
+figure_t color(figure_t f, color_t c);
+figure_t translate(figure_t f, double dx, double dy);
+figure_t rotate(figure_t f, double dt);
+color_t intersect(coord_t p, figure_t f, double grain);
+
 
 
 
